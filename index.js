c9d79a647e329b760ddcd5737df8c3ae2d52a287
satyamshorrf <satyamshorrf@gmail.com>
2025-01-12 00:55:16 +0530

    CPU Intensive Work 6 Threads use

--- index.js
+++ index.js
@@ -32,6 +32,14 @@ fs.readFile('test.txt', 'utf8', () => {
     console.log(`${Date.now() - start}ms`,'Password 4 Done')
     })
 
+    crypto.pbkdf2('password5', 'salt1', 100000, 1024, 'sha512', () => {
+    console.log(`${Date.now() - start}ms`,'Password 5 Done')
+    })
+
+    crypto.pbkdf2('password6', 'salt1', 100000, 1024, 'sha512', () => {
+    console.log(`${Date.now() - start}ms`,'Password 6 Done')
+    })
+
 });
 
 console.log('Hello from the top-level code')
