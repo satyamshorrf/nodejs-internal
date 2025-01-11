9f76de227256269bfc0ea84ce8dae644cf3bd118
satyamshorrf <satyamshorrf@gmail.com>
2025-01-12 00:25:58 +0530

    IO Polling Finish

--- index.js
+++ index.js
@@ -4,6 +4,15 @@ setTimeout(() => console.log ('Hello from Timer 1'), 0);
 
 setImmediate(() => console.log ('Hello from Immediate Fn 1'));
 
+fs.readFile('test.txt', 'utf8', () => {
+    console.log('IO Polling Finish')
+
+    setTimeout(() => console.log ('Hello from Timer 2'), 0);
+    setTimeout(() => console.log ('Hello from Timer 3'), 5 * 1000);
+
+    setImmediate(() => console.log ('Hello from Immediate Fn 2'));
+});
+
 console.log('Hello from the top-level code')
 
  
