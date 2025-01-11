d402d340eac55612d3b270207dd5eb6a2d73422b
satyamshorrf <satyamshorrf@gmail.com>
2025-01-12 00:11:49 +0530

    IO Polling

--- index.js
+++ index.js
@@ -2,6 +2,8 @@ const fs = require('fs');
 
 setTimeout(() => console.log ('Hello from Timer 1'), 0);
 
+setImmediate(() => console.log ('Hello from Immediate Fn 1'));
+
 console.log('Hello from the top-level code')
 
  
